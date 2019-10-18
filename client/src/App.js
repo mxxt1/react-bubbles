0b78f969f49c02e817b7f9edb407b8b2e08747d3
mxxt1 <matt@dustcloudlabs.com>
2019-10-18 16:26:12 -0400

    I forgot to add Route for /login, but used /login as my redirect when token not present

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import PrivateRoute from './utils/PrivateRoute.js';
 import BubblePage from './components/BubblePage.js';
 
+
 import Login from "./components/Login";
 import "./styles.scss";
 
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <PrivateRoute exact path='/bubbles-page' component={BubblePage} />
         <Route exact path="/" component={Login} />
+        <Route exact path="/login" component={Login} />
       </Switch>  
       </div>
   );
