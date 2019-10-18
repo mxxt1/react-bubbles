bc8065585b0cb65d443c0cdc4456e3be22f8ec3b
mxxt1 <matt@dustcloudlabs.com>
2019-10-18 12:11:09 -0400

    added login form, form getting data correctly. Added axiosWithAuth and PrivateRoute components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,17 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import PrivateRoute from './utils/PrivateRoute.js';
+import BubblePage from './components/BubblePage'
 
 import Login from "./components/Login";
 import "./styles.scss";
 
 function App() {
   return (
-    <Router>
       <div className="App">
+        <PrivateRoute path='bubbles-page' component={BubblePage} />
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
       </div>
-    </Router>
   );
 }
 
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,57 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axiosWithAuth from '../utils/axiosWithAuth';
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+const Login = (props) => {
+  console.log(props);
+
+  const [credentials, setCredentials] = useState({
+      username: '',
+      password: ''
+  });
+
+
+    const handleChange = e => {
+      setCredentials({
+        ...credentials,
+        [e.target.name]: e.target.value
+      })
+    }
+
+
+    const handleSubmit = e => {
+      e.preventDefault();
+
+      axiosWithAuth().post("/api/login",credentials)
+      .then(response => {
+        // console.log(response);
+        localStorage.setItem('token', response.data.payload);
+      })
+      .then(() => console.log(localStorage.token))
+      .catch(err => console.log(err))
+    }
+
+
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type='text'
+          name='username'
+          placeholder='Username'
+          value={credentials.username}
+          onChange={handleChange}
+        />
+        <input 
+        type='text'
+        name='password'
+        placeholder='Password'
+        value={credentials.password}
+        onChange={handleChange}
+        />
+      <button>Login</button>
+      </form>
     </>
   );
 };
